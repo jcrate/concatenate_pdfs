8952d1d0010dcfc6e03c1fae7f0900b68f92a1ed
Jim Crate <jimcfl@gmail.com>
2023-08-02 14:07:27 -0400

    updating build process, add code signing

--- build_release.sh
+++ build_release.sh
@@ -1,3 +1,29 @@
+#!/usr/bin/env bash
 
+cd "${BASH_SOURCE%/*}" || exit
+
+# cleanup
 rm -rf .build
+rm -rf ./built_product/
+mkdir -p ./built_product
+
+# build
 swift build -c release --arch arm64 --arch x86_64
+
+# check for built product
+binaryPath=".build/apple/Products/Release/concatenate_pdfs"
+if [[ ! -f ${binaryPath} ]]
+then
+  echo "ERROR: compiled binary not found at ${binaryPath}"
+  exit 127
+fi
+
+# copy binary to built products
+cp ${binaryPath} ./built_product/
+binaryPath="./built_product/concatenate_pdfs"
+
+# codesign
+./codesign/sign.sh "${binaryPath}"
+
+
+echo "Copy built product from ${binaryPath}"
---
+++ codesign/sign.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+scriptDir="${BASH_SOURCE%/*}"
+echo "script directory: ${scriptDir}"
+echo "signing ${1}"
+
+# make sure user specified path to binary
+if [[ $# -lt  1 || ! -f $1 ]]
+then
+  echo "ERROR: no binary specified for signing"
+  echo 1>&2 "Usage: $0 binary_to_sign"
+  exit 127
+fi
+
+binaryPath=$1
+
+if [[ ! -f "${scriptDir}/developer_id.env" ]]
+then
+  echo "ERROR: developer_id.env does not exist. Rename developer_id.env.example and specify your developer ID."
+  exit 127
+fi
+
+dev_id="sourced from developer_id.env"
+source "${scriptDir}/developer_id.env"
+echo "sourced developer id: ${dev_id}"
+
+echo "signing ${binaryPath}"
+# clean up by removing file system extended attributes
+xattr -cr "${binaryPath}"
+  
+  
+entPath="${scriptDir}/signing.entitlements"
+codesign --force --deep --verbose --options=runtime --entitlements ${entPath} --sign "${dev_id}" "${binaryPath}"
+  
+# check code-signing
+echo "checking signing"
+spctl -av "${binaryPath}"
+codesign --verify -v "${binaryPath}"
+
+# zip for notarization
+echo "zipping for notarization"
+zipPath="${binaryPath}.zip"
+if [[ -f "${zipPath}" ]]
+then
+  rm "${zipPath}"
+fi
+ditto -c -k --sequesterRsrc --keepParent "${binaryPath}" "${zipPath}"
+
+
+echo "uploading for notarization with credentials ${notarytool_credentials}"
+xc_output=$( \
+  xcrun notarytool submit --wait ${notarytool_credentials} "${zipPath}"
+)
+echo "finished uploading"
+echo "${xc_output}"
+
+# stapling an executable doesn't work
+# echo "stapling"
+# xcrun stapler staple "${binaryPath}" -v
+# rm "${zipPath}"
+# ditto -c -k --sequesterRsrc --keepParent "${binaryPath}" "${zipPath}"
+#
+# echo "checking notarization"
+# spctl -av "${binaryPath}"
+
+
